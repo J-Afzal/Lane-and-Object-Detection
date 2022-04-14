fb60404b67906232b84263b8ada560ed34203975
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-14 02:16:16 +0100

    Create main.cpp

---
+++ tests/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Junaid Afzal
+ * @brief Executes performance tests
+ * @version 1.0
+ * @date 14-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "helpers/PerformanceTests.hpp"
+
+int main()
+{
+    PerformanceTests PerformanceTests("../../resources/benchmark.mp4");
+    PerformanceTests.Run();
+}
