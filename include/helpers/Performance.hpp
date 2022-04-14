b0741375a9b4bef9e66f0361c738d5d5ba97c3ca
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-14 02:15:08 +0100

    performance

---
+++ include/helpers/Performance.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file Performance.hpp
+ * @author Junaid Afzal
+ * @brief Calculates frame times and FPS
+ * @version 1.0
+ * @date 14-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+#include "pch.hpp"
+
+class Performance
+{
+public:
+    Performance() = default;
+
+    ~Performance() = default;
+
+    void Setup(const bool &RecordFrameTimes);
+
+    void Start_Timer();
+
+    void Stop_Timer();
+
+    void Print_FPS_To_Frame(cv::Mat &Frame);
+
+    std::vector<int> Get_Frame_Times();
+
+private:
+    std::vector<int> m_FrameTimes;
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTime;
+    unsigned long long m_FrameCount = 0;
+    double m_CurrentFPS = 0, m_AverageFPS = 0;
+    bool m_RecordFrameTimes{};
+
+    const int m_FONT_FACE = cv::FONT_HERSHEY_DUPLEX, m_FONT_THICKNESS = 1, m_FONT_SCALE = 1;
+    const cv::Rect m_FPS_RECT = cv::Rect(25, 25, 350, 100);
+};
---
+++ source/helpers/Performance.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file Performance.cpp
+ * @author Junaid Afzal
+ * @brief Implementation of Performance.hpp
+ * @version 1.0
+ * @date 14-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "helpers/Performance.hpp"
+
+void Performance::Setup(const bool &RecordFrameTimes) { m_RecordFrameTimes = RecordFrameTimes; }
+
+void Performance::Start_Timer() { m_StartTime = std::chrono::high_resolution_clock::now(); }
+
+void Performance::Stop_Timer()
+{
+    if (m_RecordFrameTimes)
+    {
+        m_FrameTimes.push_back(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - m_StartTime).count());
+        m_CurrentFPS = 1000000 / (double)m_FrameTimes.back();
+    }
+    else
+        m_CurrentFPS = 1000000 / (double)std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - m_StartTime).count();
+
+    // Calculate Average FPS
+    m_FrameCount++;
+    if (m_FrameCount != 1)
+        m_AverageFPS = (m_AverageFPS * (double)(m_FrameCount - 2) + m_CurrentFPS) / (double)(m_FrameCount - 1);
+}
+
+void Performance::Print_FPS_To_Frame(cv::Mat &Frame)
+{
+    // Display the previous average FPS and previous frame FPS
+    cv::rectangle(Frame, m_FPS_RECT, cv::Scalar(0), cv::FILLED);
+
+    std::stringstream ss1;
+    ss1 << std::fixed << std::setprecision(2) << m_AverageFPS;
+    std::string FPSText = "Average FPS: " + ss1.str();
+    int baseline = 0;
+    cv::Size textSize = cv::getTextSize(FPSText, m_FONT_FACE, m_FONT_SCALE, m_FONT_THICKNESS, &baseline);
+    baseline += m_FONT_THICKNESS;
+    cv::Point textOrg = cv::Point(30, 25 + baseline + textSize.height);
+    cv::putText(Frame, FPSText, textOrg, m_FONT_FACE, m_FONT_SCALE, cv::Scalar::all(255), m_FONT_THICKNESS, cv::LINE_AA);
+
+    std::stringstream ss2;
+    ss2 << std::fixed << std::setprecision(2) << m_CurrentFPS;
+    FPSText = "Current FPS: " + ss2.str();
+    baseline = 0;
+    textSize = cv::getTextSize(FPSText, m_FONT_FACE, m_FONT_SCALE, m_FONT_THICKNESS, &baseline);
+    baseline += m_FONT_THICKNESS;
+    textOrg = cv::Point(30, 75 + baseline + textSize.height);
+    cv::putText(Frame, FPSText, textOrg, m_FONT_FACE, m_FONT_SCALE, cv::Scalar::all(255), m_FONT_THICKNESS, cv::LINE_AA);
+}
+
+std::vector<int> Performance::Get_Frame_Times() { return m_FrameTimes; }
