0b061d0589b19a37513bf569a7151d8abe3e3dc9
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-14 02:15:57 +0100

    Create Enumerations.hpp

---
+++ include/helpers/Enumerations.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file Enumerations.hpp
+ * @author Junaid Afzal
+ * @brief Custom enumerations
+ * @version 1.0
+ * @date 14-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+namespace Enumerations
+{
+    enum class Detector { NONE=0, STANDARD=1, TINY=2 };
+
+    enum class BackEnd { NONE=0, CPU=0, CUDA=1 };
+
+    enum class BlobSize { ZERO=0, ONE=288, TWO=320, THREE=416, FOUR=512, FIVE=608 };
+}
