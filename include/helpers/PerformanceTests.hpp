6419fc396a9a4d89370cb60723149a74781082d8
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-14 02:15:23 +0100

    performance tests

---
+++ include/helpers/PerformanceTests.hpp
@@ -0,0 +1,129 @@
+/**
+ * @file PerformanceTests.hpp
+ * @author Junaid Afzal
+ * @brief Performs the performance tests. First the source code is
+ * ran without an object detector as a control. Then the following
+ * three variables will be tested: YOLOv4 vs YOLOv4-tiny, blob
+ * sizes, and CUDA vs CPU
+ * @version 1.0
+ * @date 14-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+#include "pch.hpp"
+#include "helpers/Enumerations.hpp"
+
+class PerformanceTests
+{
+public:
+        explicit PerformanceTests(std::string FilePath);
+
+        ~PerformanceTests() = default;
+
+        void Run();
+
+private:
+        std::vector<std::optional<std::vector<int>>> m_FrameTimes;
+        std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTime;
+        std::string m_FilePath;
+
+        const int m_NUMBER_OF_TESTS = 21, m_NUMBER_OF_REPETITIONS = 5;
+
+        const std::vector<Enumerations::Detector> m_YOLO_TYPES = {
+            Enumerations::Detector::NONE,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::TINY,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD,
+            Enumerations::Detector::STANDARD};
+
+        const std::vector<Enumerations::BackEnd> m_BACK_END_TYPES = {
+            Enumerations::BackEnd::NONE,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CUDA,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+            Enumerations::BackEnd::CPU,
+        };
+
+        const std::vector<Enumerations::BlobSize> m_BLOB_SIZES = {
+            Enumerations::BlobSize::ZERO,
+            Enumerations::BlobSize::ONE,
+            Enumerations::BlobSize::TWO,
+            Enumerations::BlobSize::THREE,
+            Enumerations::BlobSize::FOUR,
+            Enumerations::BlobSize::FIVE,
+            Enumerations::BlobSize::ONE,
+            Enumerations::BlobSize::TWO,
+            Enumerations::BlobSize::THREE,
+            Enumerations::BlobSize::FOUR,
+            Enumerations::BlobSize::FIVE,
+            Enumerations::BlobSize::ONE,
+            Enumerations::BlobSize::TWO,
+            Enumerations::BlobSize::THREE,
+            Enumerations::BlobSize::FOUR,
+            Enumerations::BlobSize::FIVE,
+            Enumerations::BlobSize::ONE,
+            Enumerations::BlobSize::TWO,
+            Enumerations::BlobSize::THREE,
+            Enumerations::BlobSize::FOUR,
+            Enumerations::BlobSize::FIVE,
+        };
+
+        const std::vector<std::string> m_OUTPUT_FILE_NAMES = {
+            "no_yolov4.txt",
+            "yolov4-tiny_288_cuda.txt",
+            "yolov4-tiny_320_cuda.txt",
+            "yolov4-tiny_416_cuda.txt",
+            "yolov4-tiny_512_cuda.txt",
+            "yolov4-tiny_608_cuda.txt",
+            "yolov4_288_cuda.txt",
+            "yolov4_320_cuda.txt",
+            "yolov4_416_cuda.txt",
+            "yolov4_512_cuda.txt",
+            "yolov4_608_cuda.txt",
+            "yolov4-tiny_288.txt",
+            "yolov4-tiny_320.txt",
+            "yolov4-tiny_416.txt",
+            "yolov4-tiny_512.txt",
+            "yolov4-tiny_608.txt",
+            "yolov4_288.txt",
+            "yolov4_320.txt",
+            "yolov4_416.txt",
+            "yolov4_512.txt",
+            "yolov4_608.txt",
+        };
+};
---
+++ source/helpers/PerformanceTests.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file PerformanceTests.cpp
+ * @author Junaid Afzal
+ * @brief Implementation of PerformanceTests.hpp
+ * @version 1.0
+ * @date 14-04-2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "helpers/PerformanceTests.hpp"
+#include "helpers/VideoManager.hpp"
+
+PerformanceTests::PerformanceTests(std::string FilePath) : m_FilePath(std::move(FilePath)) {}
+
+void PerformanceTests::Run()
+{
+    std::cout << "\n\nNumber of tests: " << m_NUMBER_OF_TESTS << "\nNumber of repetitions: " << m_NUMBER_OF_REPETITIONS << '\n';
+
+    m_StartTime = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < m_NUMBER_OF_TESTS; i++)
+    {
+        m_FrameTimes.clear();
+
+        for (int j = 0; j < m_NUMBER_OF_REPETITIONS; j++)
+        {
+            VideoManager Vid(m_FilePath, m_YOLO_TYPES[i], m_BACK_END_TYPES[i], m_BLOB_SIZES[i], true);
+            m_FrameTimes.push_back(Vid.Run());
+
+            std::cout << m_OUTPUT_FILE_NAMES[i] << ": Finished iteration " << j + 1 << "/" << m_NUMBER_OF_REPETITIONS << " for test " << i + 1 << "/" << m_NUMBER_OF_TESTS << '\n';
+        }
+
+        std::ofstream OutputFile(m_OUTPUT_FILE_NAMES[i]);
+        if (!OutputFile.is_open())
+        {
+            std::cout << "\nError opening output file: " + m_OUTPUT_FILE_NAMES[i] + '\n';
+            exit(1);
+        }
+
+        for (int j = 0; j < m_FrameTimes[0]->size(); j++)
+        {
+            int FrameTimeSum = 0;
+            for (const auto &FrameTime : m_FrameTimes)
+                FrameTimeSum += FrameTime->at(j);
+
+            OutputFile << std::to_string(FrameTimeSum / (double)m_NUMBER_OF_REPETITIONS) << '\n';
+        }
+    }
+
+    int total = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - m_StartTime).count();
+    double hours = total / 3600.0;
+    double minutes = (hours - floor(hours)) * 60;
+    double seconds = (minutes - floor(minutes)) * 60;
+
+    hours = floor(hours);
+    minutes = floor(minutes);
+    seconds = floor(seconds);
+
+    std::cout << "\n\nAll test complete!\nTotal time taken = " << hours << "hrs " << minutes << "mins " << seconds << "secs\n\n";
+}
