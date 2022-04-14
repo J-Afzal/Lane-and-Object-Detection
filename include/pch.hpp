7ebc026111079071591e1031e388eedb494693d3
Junaid Afzal <Junaid-Afzal@Outlook.com>
2022-04-14 02:13:27 +0100

    Create pch.hpp

---
+++ include/pch.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+/** std **/
+#include <chrono>
+#include <exception>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+/** data containers **/
+#include <vector>
+#include <deque>
+
+/** opencv **/
+#include "opencv2/dnn.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
